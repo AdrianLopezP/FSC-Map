567bfa427e4b1b33aa311efffced2f4ff820dff2
Adrian Lopez <adrianlopez.pr23@gmail.com>
2021-12-05 11:35:28 -0500

    Added \n to names to make them more compact when displayed, having problems displaying a pin image over the location

--- CampusMap2.py
+++ CampusMap2.py
@@ -6,38 +6,69 @@ from scipy.spatial.distance import cdist, squareform
 IMAGEFILE = 'fsc_campus_map.png'
 WIDTH = 985
 HEIGHT = 525
-COLORS = {'txt': '#000000'}
+COLORS = {'txt': '#FF0000', 'bg': '#FFFFFF'}
 THOLD = 40
 FONT = ("Consolas", 10, "bold")
-NAMES = ['Barnett Athletic Complex', 'Badcock Memorial Garden','Mr. George\'s Green', 'Water Dome',
-        'Dell Hall','Hollis Hall','Miller Hall','Allan Spivey Hall','Joseph Reynolds Hall',
-        'Wesley Hall','Nicholas Hall','Buck Stop Grill (Pizza)','Happy Place','The Caf','Greenhouse',
-        'President\'s Residence','George W. Jenkins Field House','Fannin Center','Ruel B. Gilbert Gymnasium',
-        'Campus Safety and Security','Edge Hall','Jack M. Berry Citrus Building',
-        'Volleyball Courts','Volleyball Courts','Intramurals Field','Office of Marketing and Communication',
-        'Evett Simmons Multicultural Appreciation Center',
-        'William F. Chatlos Journalism Building','Joe K. and Alberta Blanton Nursing Building',
-        'Wynee Warden Dance Studio','Dr. Marcene H. & Robert E. Christoverson Humanities Building',
-        'Sarah D. & L. Kirk McKay, Jr. Archives Center','Roux Library (Tutu\'s Cafe)',
-        'Emile E. Watson Administration Building','Benjamin Fine Administration Building',
-        'Thad Buckner Building','Annie Pfeiffer Chapel','William H. Danforth Chapel',
-        'Robert A. Davis Performing Arts Center','Honeyman Pavilion',
-        'Brandscomb Memorial Auditorium','Music Addition','Marjorie McKinley Music Building',
-        'Melvin Gallery','Hester Plaza','Loca Lee Buckner Theatre','President\'s Residence',
-        'Planetarium','Polk County Science Building','Polk County Science Building',
-        'Polk County Science Building','Polk County Science Building',
-        'Centennial Tower and Presidential Garden Plaza','Carlisle Rogers Building',
-        'L.A. Raulerson Building','Sharp Family Tourism & Education Center Usonian House',
-        'Rogers Field','Willis Garden of Meditation','Patriot\'s Plaza',
-        'Lucius Pond Ordway Building','Military Science','L.N. Pipkin Bandshell',
-        'Barnett Early Childhood Learning and Health','Roberts Academy',
-        'Greek Village (Jenkins Hall)','Greek Village (Jenkins Hall)',
-        'Wynee Warden Tennis Center','Wynee Warden Tennis Center',
-        'Rinker Technology Center','Swimming Pool','Lynn\'s Garden',
-        'Boathouse','Nina B. Hollis Wellness Center',
-        'Charles T. Thrift Building (Student Health & Counseling Centers)',
-        'Admissions Center','Facilities Maintenance Building',
-        'Becker Business Building','Weinstein Computer Sciences Center']
+# NAMES = ['Barnett Athletic Complex','Badcock Memorial Garden','Mr. George\'s Green','Water Dome',
+#         'Dell Hall','Hollis Hall','Miller Hall','Allan Spivey Hall','Joseph Reynolds Hall',
+#         'Wesley Hall','Nicholas Hall','Buck Stop Grill (Pizza)','Happy Place','The Caf','Greenhouse',
+#         'President\'s Residence','George W. Jenkins Field House','Fannin Center','Ruel B. Gilbert Gymnasium',
+#         'Campus Safety and Security','Edge Hall','Jack M. Berry Citrus Building',
+#         'Volleyball Courts','Volleyball Courts','Intramurals Field','Office of Marketing and Communication',
+#         'Evett Simmons Multicultural Appreciation Center',
+#         'William F. Chatlos Journalism Building','Joe K. and Alberta Blanton Nursing Building',
+#         'Wynee Warden Dance Studio','Dr. Marcene H. & Robert E. Christoverson Humanities Building',
+#         'Sarah D. & L. Kirk McKay, Jr. Archives Center','Roux Library (Tutu\'s Cafe)',
+#         'Emile E. Watson Administration Building','Benjamin Fine Administration Building',
+#         'Thad Buckner Building','Annie Pfeiffer Chapel','William H. Danforth Chapel',
+#         'Robert A. Davis Performing Arts Center','Honeyman Pavilion',
+#         'Brandscomb Memorial Auditorium','Music Addition','Marjorie McKinley Music Building',
+#         'Melvin Gallery','Hester Plaza','Loca Lee Buckner Theatre','President\'s Residence',
+#         'Planetarium','Polk County Science Building','Polk County Science Building',
+#         'Polk County Science Building','Polk County Science Building',
+#         'Centennial Tower and Presidential Garden Plaza','Carlisle Rogers Building',
+#         'L.A. Raulerson Building','Sharp Family Tourism & Education Center Usonian House',
+#         'Rogers Field','Willis Garden of Meditation','Patriot\'s Plaza',
+#         'Lucius Pond Ordway Building','Military Science','L.N. Pipkin Bandshell',
+#         'Barnett Early Childhood Learning and Health','Roberts Academy',
+#         'Greek Village (Jenkins Hall)','Greek Village (Jenkins Hall)',
+#         'Wynee Warden Tennis Center','Wynee Warden Tennis Center',
+#         'Rinker Technology Center','Swimming Pool','Lynn\'s Garden',
+#         'Boathouse','Nina B. Hollis Wellness Center',
+#         'Charles T. Thrift Building (Student Health & Counseling Centers)',
+#         'Admissions Center','Facilities Maintenance Building',
+#         'Becker Business Building','Weinstein Computer Sciences Center']
+
+NAMES = ['Barnett\nAthletic\nComplex','Badcock\nMemorial\nGarden','Mr. George\'s\nGreen','Water\nDome',
+    'Dell Hall','Hollis Hall','Miller Hall','Allan Spivey\nHall','Joseph\nReynolds\nHall',
+    'Wesley\nHall','Nicholas\nHall','Buck\nStop\nGrill\n(Pizza)','Happy\nPlace','The Caf','Greenhouse',
+    'President\'s\nResidence','George W.\nJenkins\nField\nHouse','Fannin\nCenter','Ruel B.\nGilbert\nGymnasium',
+    'Campus\nSafety\nand\nSecurity','Edge\nHall','Jack\nM.\nBerry\nCitrus\nBuilding',
+    'Volleyball\nCourts','Volleyball\nCourts','Intramurals\nField','Office of\nMarketing\nand\nCommunication',
+    'Evett\nSimmons\nMulticultural\nAppreciation\nCenter',
+    'William F.\nChatlos\nJournalism\nBuilding','Joe K. and\nAlberta\nBlanton\nNursing\nBuilding',
+    'Wynee\nWarden\nDance\nStudio','Dr. Marcene H. &\nRobert E.\nChristoverson\nHumanities\nBuilding',
+    'Sarah D. &\nL. Kirk McKay, Jr.\nArchives\nCenter','Roux Library\n(Tutu\'s\nCafe)',
+    'Emile\nE.\nWatson\nAdministration\nBuilding','Benjamin\nFine\nAdministration\nBuilding',
+    'Thad\nBuckner\nBuilding','Annie\nPfeiffer\nChapel','William\nH.\nDanforth\nChapel',
+    'Robert\nA.\nDavis\nPerforming\nArts\nCenter','Honeyman\nPavilion',
+    'Brandscomb\nMemorial\nAuditorium','Music\nAddition','Marjorie\nMcKinley\nMusic\nBuilding',
+    'Melvin\nGallery','Hester\nPlaza','Loca\nLee\nBuckner\nTheatre','President\'s\nResidence',
+    'Planetarium','Polk\nCounty\nScience\nBuilding','Polk\nCounty\nScience\nBuilding',
+    'Polk\nCounty\nScience\nBuilding','Polk\nCounty\nScience\nBuilding',
+    'Centennial\nTower\nand\nPresidential\nGarden\nPlaza','Carlisle\nRogers\nBuilding',
+    'L.A.\nRaulerson\nBuilding','Sharp\nFamily\nTourism\n&\nEducation\nCenter\nUsonian\nHouse',
+    'Rogers\nField','Willis\nGarden\nof\nMeditation','Patriot\'s\nPlaza',
+    'Lucius\nPond\nOrdway\nBuilding','Military\nScience','L.N.\nPipkin\nBandshell',
+    'Barnett\nEarly\nChildhood\nLearning\nand\nHealth','Roberts\nAcademy',
+    'Greek\nVillage\n(Jenkins\nHall)','Greek\nVillage\n(Jenkins\nHall)',
+    'Wynee\nWarden\nTennis\nCenter','Wynee\nWarden\nTennis\nCenter',
+    'Rinker\nTechnology\nCenter','Swimming\nPool','Lynn\'s\nGarden',
+    'Boathouse','Nina\nB.\nHollis\nWellness\nCenter',
+    'Charles\nT.\nThrift\nBuilding\n(Student\nHealth\n&\nCounseling\nCenters)',
+    'Admissions\nCenter','Facilities\nMaintenance\nBuilding',
+    'Becker\nBusiness\nBuilding','Weinstein\nComputer\nSciences\nCenter']
+
 COORDINATES = [(512, 113),(407, 183),(540, 261),
         (221, 165),(722, 209),(717, 159),(700, 182),
         (466, 264),(398, 262),(443, 402),(527, 389),
@@ -76,17 +107,15 @@ def guisetup():
     y = (root.winfo_screenheight() - HEIGHT) // 2
     root.geometry(f"{WIDTH}x{HEIGHT}+{x}+{y}")  # adjust the size of the window
     root.resizable(False, False)  # make the window fixed
-
     canvas = Canvas(root, width=WIDTH, height=HEIGHT, borderwidth=0)
     canvas.pack()
-
     return root, canvas
 
 def getorigin(event, buildings, canvas):
+    canvas.delete('text', 'back')
     pt = np.array([event.x, event.y]).reshape(-1, 2)
     bldg = np.array(list(buildings.values()))
     d = cdist(pt, bldg)[0]
-
     smallest = min(d)
     if smallest < THOLD:
         buildingIndex = np.argmin(d)
@@ -96,9 +125,14 @@ def getorigin(event, buildings, canvas):
 
 def click(canvas, name, coordinates):
     print("Click works! (", coordinates, ")", sep="")
-    canvas.delete('text', 'back')
-    text = canvas.create_text(coordinates[0], coordinates[1] + 20, text=name, anchor=CENTER, fill=COLORS['txt'], font=FONT, tag='text')
-    back = canvas.create_rectangle(canvas.bbox(text),fill="white", tag='back')
+    
+    # I want a pin to show up exactly where the coordinates of the building is.
+    # However, it is not showing up.
+    pin = ImageTk.PhotoImage(Image.open('map-pin.jpg'))
+    canvas.create_image(coordinates[0], coordinates[1], image=pin, anchor=S, tag='pin')
+    
+    text = canvas.create_text(coordinates[0], coordinates[1] + 20, justify=CENTER, text=name, anchor=CENTER, fill=COLORS['txt'], font=FONT, tag='text')
+    back = canvas.create_rectangle(canvas.bbox(text),fill=COLORS['bg'], tag='back')
     canvas.tag_lower(back, text)
 
 def endgame(event):

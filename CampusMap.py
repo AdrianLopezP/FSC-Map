f5b82bf78db2cf4894bd4b3b0772b34b25b807eb
Adrian Lopez <94017797+AdrianLopezP@users.noreply.github.com>
2023-11-04 21:42:35 -0400

    Cleaned the code in CampusMap.py

--- CampusMap.py
+++ CampusMap.py
@@ -1,16 +1,17 @@
 from tkinter import *
 from PIL import ImageTk, Image
 import numpy as np
-from scipy.spatial.distance import cdist, squareform
+from scipy.spatial.distance import cdist
 
+# Constants
 IMAGEFILE = {'map': 'fsc_campus_map.png', 'pin': 'map-pin.png', 'fsc': 'FSCIMG.jpg'}
 WIDTH = 985
 HEIGHT = 525
 COLORS = {'txt': '#FF0000', 'bg': '#FFFFFF'}
-THOLD = 40
+THOLD = 40  # Threshold for distance to detect nearest location
 FONT = ("Consolas", 10, "bold")
 
-# List of all the names of the locations on campus
+# Data for campus locations
 NAMES = ['Barnett\nAthletic\nComplex','Badcock\nMemorial\nGarden','Mr. George\'s\nGreen','Water\nDome',
     'Dell Hall','Hollis Hall','Miller Hall','Allan Spivey\nHall','Joseph\nReynolds\nHall',
     'Wesley\nHall','Nicholas\nHall','Buck Stop\nGrill (Pizza)','Happy\nPlace','The Caf','Greenhouse',
@@ -41,7 +42,6 @@ NAMES = ['Barnett\nAthletic\nComplex','Badcock\nMemorial\nGarden','Mr. George\'s
     'Becker\nBusiness\nBuilding','Weinstein\nComputer\nSciences\nCenter',
     'Jenkins Hall']
 
-# List of all the coordinates of the locations on campus
 COORDINATES = [(512, 113),(407, 183),(540, 261),(221, 165),(722, 209),(717, 159),(700, 182),
         (466, 264),(398, 262),(443, 402),(527, 389),(382, 208),(832, 273),(639, 275),(386, 337),
         (317, 409),(504, 196),(452, 187),(560, 175),(626, 159),(459, 335),(510, 308),(698, 245),
@@ -53,114 +53,92 @@ COORDINATES = [(512, 113),(407, 183),(540, 261),(221, 165),(722, 209),(717, 159)
         (900, 64),(646, 93),(670, 94),(830, 140),(628, 211),(623, 332),(579, 362),
         (618, 453),(656, 365),(662, 314),(794, 347),(793, 280),(927, 366),(866, 334),(745, 88)]
 
-# Dictionary with names and coordinates of bus stops
 BUSSTOPS = {'Hollis Rooms': (742, 168),'Becker': (893, 337), 'ROTC Circle': (590, 175),
             'Weinstein': (844, 318), 'Buckner': (184, 270)}
 
 PARKING = [(891, 286),(801, 204),(698, 301),(625, 184),(551, 323),(380, 451),(106, 453),(162, 307),(144, 121),(253, 60),
             (54, 137),(49, 210),(50, 273),(320, 21),(478, 67),(338, 102),(700, 31),(741, 117),(770, 80),(108, 368)]
 
+# Main application function
 def main():
-    # Calls guisetup and catches the return into the root and canvas variables
     root, canvas = guisetup()
+    
+    # Load and display the campus map
+    map_image = ImageTk.PhotoImage(Image.open(IMAGEFILE['map']))
+    canvas.create_image(0, 0, image=map_image, anchor=NW, tag='map')
 
-    # Open map image and puts it on the the window
-    map = ImageTk.PhotoImage(Image.open(IMAGEFILE['map']))
-    canvas.create_image(0, 0, image=map, anchor=NW, tag='map')
+    # Load and prepare the pin image
+    pin_image = ImageTk.PhotoImage(Image.open(IMAGEFILE['pin']).resize((50,50)))
+    canvas.setvar('pin', pin_image)
 
-    # Open pin image and assigns it to the 'pin' variable
-    pin = ImageTk.PhotoImage(Image.open(IMAGEFILE['pin']).resize((50,50)))
-    canvas.setvar('pin', pin)
-
-    # Creates a dictionary with names as keys and coordinates as values
+    # Create a dictionary mapping building names to their coordinates
     buildings = dict(zip(NAMES, COORDINATES))
 
-    # Creates the button for bus stops
-    busButton = Button(root,text='Bus Stops',width=7,height=1,bd='1')
-    busButton['command'] = lambda arg1=canvas, arg2=BUSSTOPS : showbus(arg1, arg2)
-    busButton.place(x=5, y=5)
-    
-    # Creates the button for parking
-    parkingButton = Button(root,text='Parking',width=7,height=1,bd='1')
-    parkingButton['command'] = lambda arg1=canvas, arg2=PARKING : showParking(arg1, arg2)
-    parkingButton.place(x=5, y=35)
+    # Add GUI elements (buttons)
+    add_buttons(root, canvas, BUSSTOPS, PARKING)
 
-    # Creates button for clearing the screen
-    closeButton1 = Button(root,text='Clear',width=5,height=1,bd='1')
-    closeButton1['command'] = lambda arg1=canvas : close(arg1)
-    closeButton1.place(x=70, y=5)
-
-    # Calls getorigin whenever the mouse is clicked
+    # Bind events for mouse click and escape key
     root.bind("<Button 1>", lambda event: getorigin(event, buildings, canvas))
+    root.bind("<Escape>", lambda event: event.widget.destroy())
 
-    # Calls endgame when escape key is pressed
-    root.bind("<Escape>", endgame)
-
-    # Starts mainloop
+    # Start the GUI event loop
     root.mainloop()
 
+# GUI setup function
 def guisetup():
-
-    # Creates the root
     root = Tk()
     root.title("FSC Campus Map")
     root.iconbitmap("FSCLOGO.ico")
+
+    # Center the window on the screen
     x = (root.winfo_screenwidth() - WIDTH) // 2
     y = (root.winfo_screenheight() - HEIGHT) // 2
-    root.geometry(f"{WIDTH}x{HEIGHT}+{x}+{y}")  # adjust the size of the window
-    root.resizable(False, False)  # make the window fixed
+    root.geometry(f"{WIDTH}x{HEIGHT}+{x}+{y}")
+    root.resizable(False, False)
 
-    # Creates the canvas
+    # Create a canvas for drawing the map and elements
     canvas = Canvas(root, width=WIDTH, height=HEIGHT, borderwidth=0)
     canvas.pack()
 
-    # Returns both root and canvas as defined up above
     return root, canvas
 
+# Function to handle mouse click events
 def getorigin(event, buildings, canvas):
+    canvas.delete('text', 'back', 'pin')  # Clear previous labels and pins
 
-    # Deletes any pre-existing text, textbox, or pins on the window
-    canvas.delete('text', 'back', 'pin')
+    # Compute distances from the click to each building
+    click_point = np.array([event.x, event.y]).reshape(-1, 2)
+    building_points = np.array(list(buildings.values()))
+    distances = cdist(click_point, building_points)[0]
+    nearest_building_distance = min(distances)
 
-    # Creates a numpy array consisting of the coordinate of the click
-    pt = np.array([event.x, event.y]).reshape(-1, 2)
+    # Check if the click is close enough to a building
+    if nearest_building_distance < THOLD:
+        building_index = np.argmin(distances)
+        building_name = list(buildings)[building_index]
+        click(canvas, building_name, list(buildings.values())[building_index])
 
-    # Creates a numpy array consisting of all of the coordinates of the locations on the map
-    bldg = np.array(list(buildings.values()))
-
-    # Finds the distances between the click and each one of the locations on the map
-    d = cdist(pt, bldg)[0]
-
-    # Find the smallest of these distances
-    smallest = min(d)
-
-    # Checks if the closet location to the click is within a predetermined threshold
-    if smallest < THOLD:
-
-        # Gets the index and name (key) of the location
-        buildingIndex = np.argmin(d)
-        buildingName = list(buildings)[buildingIndex]
-        
-        # Calls click
-        click(canvas, list(buildings.keys())[buildingIndex], list(buildings.values())[buildingIndex])
-
+# Function to handle the display of a pin and label
 def click(canvas, name, coordinates):
-    
-    # Creates the pin image exactly on top of the coordinates of the location
+    # Place the pin
     canvas.create_image(coordinates[0], coordinates[1]+5, image=canvas.getvar('pin'), anchor=S, tag='pin')
 
-    # Checks if the location is Christoverson in order to show the text and textbox in a different spot and ensure its on the screen
-    if (coordinates == ((103, 481))):
-        text = canvas.create_text(coordinates[0], coordinates[1] - 100, justify=CENTER, text=name, anchor=N, fill=COLORS['txt'], font=FONT, tag='text')
+    # Decide the location for the label based on the specific building
+    if coordinates == ((103, 481)):
+        text_anchor = (coordinates[0], coordinates[1] - 100)
     else:
-        text = canvas.create_text(coordinates[0], coordinates[1] + 20, justify=CENTER, text=name, anchor=N, fill=COLORS['txt'], font=FONT, tag='text')
-    
-    # Adds a textbox and background to the text
-    back = canvas.create_rectangle(canvas.bbox(text),fill=COLORS['bg'], tag='back')
+        text_anchor = (coordinates[0], coordinates[1] + 20)
+
+    # Create the label
+    text = canvas.create_text(text_anchor, justify=CENTER, text=name, anchor=N, fill=COLORS['txt'], font=FONT, tag='text')
+
+    # Create a background box for the label
+    back = canvas.create_rectangle(canvas.bbox(text), fill=COLORS['bg'], tag='back')
     canvas.tag_lower(back, text)
 
+# Functions to display bus stops and parking spots
 def showbus(canvas, BUSSTOPS):
-
+    
     # Creates Bus Stops title
     canvas.create_text(470, 18, justify=CENTER, text='Bus Stops', anchor=CENTER, fill=COLORS['txt'], font=("Consolas", 22, "bold"), tag='text')
 
@@ -178,7 +156,7 @@ def showbus(canvas, BUSSTOPS):
         canvas.tag_lower(back, text)
 
 def showParking(canvas, PARKING):
-
+    
     # Creates Parking title
     canvas.create_text(470, 18, justify=CENTER, text='Parking', anchor=CENTER, fill=COLORS['txt'], font=("Consolas", 22, "bold"), tag='text')
     
@@ -191,14 +169,27 @@ def showParking(canvas, PARKING):
         # Displays pin image at the coordinates of a parking spot
         canvas.create_image(coordinates[0], coordinates[1]+5, image=canvas.getvar('pin'), anchor=S, tag='pin')
 
+# Function to clear pins and labels from the canvas
 def close(canvas):
-
-    # Deletes any pin, text, and textbox on the screen
     canvas.delete('text', 'back', 'pin')
 
-def endgame(event):
+# Function to add buttons to the GUI
+def add_buttons(root, canvas, BUSSTOPS, PARKING):
+    # Bus stop button
+    busButton = Button(root, text='Bus Stops', width=7, height=1, bd='1')
+    busButton['command'] = lambda: showbus(canvas, BUSSTOPS)
+    busButton.place(x=5, y=5)
 
-    # Ends the program
-    event.widget.destroy()
+    # Parking button
+    parkingButton = Button(root, text='Parking', width=7, height=1, bd='1')
+    parkingButton['command'] = lambda: showParking(canvas, PARKING)
+    parkingButton.place(x=5, y=35)
 
-main()
+    # Clear button
+    closeButton = Button(root, text='Clear', width=5, height=1, bd='1')
+    closeButton['command'] = lambda: close(canvas)
+    closeButton.place(x=70, y=5)
+
+# Run the main function if the script is executed
+if __name__ == "__main__":
+    main()
